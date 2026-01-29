ff98bb9b1165aa7825c2787dadc058afbfe8bfa3
ssprasad-cyber <2040320ssprasad990@gmail.com>
2026-01-29 12:33:19 +0530

    stage 2 v2 completed moving to stage 3

--- src/main.py
+++ src/main.py
@@ -1,8 +1,9 @@
 from camera_layer.camera_factory import create_camera
 from camera_layer.camera_manager import CameraManager
-from edge_processing.frame_capture.frame_buffer import FrameBuffer
+from edge_processing.frame_capture.frame_packet import FramePacket
+from edge_processing.frame_capture.frame_dispatcher import FrameDispatcher
 from edge_processing.frame_capture.fps_controller import FPSController
-import cv2
+import time, cv2
 
 camera_configs = [
     {"id": "cam1", "type": "file", "path": "data/raw/sample.mp4"},
@@ -12,31 +13,36 @@ camera_configs = [
 cameras = [create_camera(cfg) for cfg in camera_configs]
 manager = CameraManager(cameras)
 
-buffers = {
-    cam.camera_id: FrameBuffer(max_size=20)
-    for cam in cameras
-}
+dispatcher = FrameDispatcher(
+    camera_ids=[cfg["id"] for cfg in camera_configs],
+    buffer_size=20
+)
 
-fps_ctrl = FPSController(target_fps=10)
+fps = FPSController(target_fps=10)
 
 while True:
     packets = manager.read_all()
 
-    # Push into buffers
     for pkt in packets:
-        buffers[pkt["camera_id"]].push(pkt)
+        frame_packet = FramePacket(
+            camera_id=pkt["camera_id"],
+            frame=pkt["frame"],
+            timestamp=pkt["timestamp"]
+        )
+        dispatcher.push(frame_packet)
 
-    # Controlled pop (simulate next stage)
-    for cam_id, buffer in buffers.items():
-        frame_pkt = buffer.latest()
-        
-        if frame_pkt:
-            cv2.imshow(cam_id, frame_pkt["frame"])
+        synced_frames = dispatcher.pull_latest()
 
-    fps_ctrl.wait()
+    for cam_id, pkt in synced_frames.items():
+        cv2.imshow(cam_id, pkt.frame)
+
+    fps.wait()
+
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
+
+
 manager.release_all()
-cv2.destroyAll()
+cv2.destroyAllWindows()
